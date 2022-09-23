729f8b4d523bc40b186ed84836c50d204ada10c9
pporong <inane09@gmail.com>
2022-09-23 18:54:23 +0900

    article controller 게시물 추가 구현

---
+++ src/main/java/com/gbr/exam/demo/controller/UserArticleController.java
@@ -0,0 +1,36 @@
+package com.gbr.exam.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserArticleController {
+	
+	private int lastArticleId;
+	private List<Article> articles;		
+	
+	public UserArticleController() {
+		lastArticleId = 0;
+		articles = new ArrayList<>();
+	}
+	
+	@RequestMapping("/user/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		int id = lastArticleId +1;
+		Article article = new Article(id, title, body);
+		
+		articles.add(article);
+		lastArticleId = id;
+		
+		return article;
+	}
+
+
+}
+
+
--- src/main/java/com/gbr/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/gbr/exam/demo/controller/UserHomeController.java
@@ -15,68 +15,13 @@ import lombok.NoArgsConstructor;
 
 @Controller
 public class UserHomeController {
-	// string
-	@RequestMapping("/user/home/getString")
-	@ResponseBody
-	public String getString() {
-		return "hi";
-	}
-	// int
-	@RequestMapping("/user/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 10;
-	}	
-	// float
-	@RequestMapping("/user/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 10.5f;
-	}
-	// double
-	@RequestMapping("/user/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 10.5;
-	}
-	// boolean
-	@RequestMapping("/user/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}
-	// char
-	@RequestMapping("/user/home/getChar")
-	@ResponseBody
-	public char getChar() {
-		return 'a';
-	}
-	// map
-	@RequestMapping("/user/home/getMap")
-	@ResponseBody
-	public Map getMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("철수나이", 56);
-		map.put("영희나이", 43);
-		
-		return map;
-	}
-	// list
-	@RequestMapping("/user/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("배고프다");
-		list.add("집에 가고싶다 히히");
-		
-		return list;
-	}
+
 	// article
 	@RequestMapping("/user/home/getArticle")
 	@ResponseBody
 	public Article getArticle() {
-		Article article = new Article(18,"배고픔ㅋ");
-		Article article2 = new Article();
+		Article article = new Article(1, "제목1", "내용1");
+		Article article2 = new Article(2, "제목2", "내용2");
 		
 		return article;
 	}
@@ -84,8 +29,8 @@ public class UserHomeController {
 	@RequestMapping("/user/home/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		Article article1 = new Article(18,"배고픔ㅋ");
-		Article article2 = new Article(2, "제목2");
+		Article article1 = new Article();
+		Article article2 = new Article();
 		
 		List<Article> articles = new ArrayList<>();
 		articles.add(article1);
@@ -101,4 +46,5 @@ public class UserHomeController {
 class Article {
 	private int id;
 	private String title;	
+	private String body;
 }
