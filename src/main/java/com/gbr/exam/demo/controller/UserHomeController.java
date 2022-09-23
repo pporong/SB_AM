159009767fe3052edb0f41ebbb239ef56a1f3d96
pporong <inane09@gmail.com>
2022-09-23 17:31:32 +0900

    user/home/main2,3 추가

--- src/main/java/com/gbr/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/gbr/exam/demo/controller/UserHomeController.java
@@ -11,4 +11,16 @@ public class UserHomeController {
 	public String showMain() {
 		return "하ㅓ하하하하하잉 푸헹헹";
 	}
+	
+	@RequestMapping("/user/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑소";
+	}
+	
+	@RequestMapping("/user/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "조심해서 가시오";
+	}
 }
