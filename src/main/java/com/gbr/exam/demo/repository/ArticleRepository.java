a38166d816074ae84e809479833cbc1e3251c8ff
pporong <inane09@gmail.com>
2022-09-28 17:08:37 +0900

    articleModify시 입력 된 정보만 수정될 수 있도록

--- src/main/java/com/gbr/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/gbr/exam/demo/repository/ArticleRepository.java
@@ -2,11 +2,7 @@ package com.gbr.exam.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.gbr.exam.demo.vo.Article;
 
